f00043de68751b5513c2cab233cfd7a0e740de8a
FelixGGG <cerdo99965@gmail.com>
2018-05-30 17:01:04 -0700

    lo ultimo

--- blog/views.py
+++ blog/views.py
@@ -62,10 +62,10 @@ def add_comment_to_post(request, pk):
         post = get_object_or_404(Post, pk=pk)
         if request.method == "POST":
            form = CommentForm(request.POST)
-        if form.is_valid():
-           comment = form.save(commit=False)
-           comment.post = post   
-           comment.save()
+           if form.is_valid():
+              comment = form.save(commit=False)
+              comment.post = post   
+              comment.save()
            return redirect('post_detail', pk=post.pk)
         else:
            form = CommentForm()
